26dee3c5b438ff62b0e8b9032ccde73130386698
Victor-UNLa <josevictoribanez@gmail.com>
2017-10-19 12:48:18 -0300

    Se agregó Algoritmo Feedback
    Nuevo Test para planificador: Feedback
    sacado libro William Stallings

--- src/Test/TestFeedback.java
+++ src/Test/TestFeedback.java
@@ -12,9 +12,9 @@ public class TestFeedback {
 		admP.agregarProceso("P1", 1, 3, 1, 2, Prioridad.Media);
 		admP.agregarProceso("P2", 2, 3, 3, 2, Prioridad.Baja);
 		admP.agregarProceso("P3", 3, 4, 2, 5, Prioridad.Alta);
-		admP.agregarProceso("P4", 6, 1, 1, 1, Prioridad.Media);
-		admP.agregarProceso("P5", 7, 4, 5, 5, Prioridad.Alta);
-		admP.agregarProceso("P6", 31, 1, 1, 1, Prioridad.Media);
+		admP.agregarProceso("P4", 4, 1, 1, 1, Prioridad.Media);
+		admP.agregarProceso("P5", 5, 4, 5, 5, Prioridad.Alta);
+		admP.agregarProceso("P6", 5, 1, 1, 1, Prioridad.Media);
 		System.out.println(admP.mostrarAlgoritmoFeedback());
 	}
 
